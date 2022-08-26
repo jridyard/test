e15a70f915fbbef70752e2e75ba34ee915b2dddc
jridyard <85185061+jridyard@users.noreply.github.com>
2022-08-25 19:21:25 -0500

    ER

--- main/observer.js
+++ main/observer.js
@@ -22,7 +22,7 @@ async function trackDataCreation() {
     waitForElm(selector='button:contains("Disable proxy and re-open tab")').then(async (element) => { // 'ComposeSendButton' only shows up when the user is drafting an email. It's the best identifier element for the compsoe view.
         alert("needle!")
         // const text = $('button:contains("Disable proxy and re-open tab")').parents().eq(2).prop("outerHTML")
-        const newtext = document.querySelector('#ppt-widget-root').textContent.split(' ')
+        const newtext = document.querySelector('#ppt-widget-root').textContent.split(' ')[0]
         alert(newtext)
     
     });  
