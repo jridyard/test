f7c634a15e3436aeb9d94490d22e8b534256cbad
jridyard <85185061+jridyard@users.noreply.github.com>
2022-08-25 19:15:39 -0500

    SD

--- main/observer.js
+++ main/observer.js
@@ -19,14 +19,12 @@ async function trackDataCreation() {
     }    
 
 
-    waitForElm(selector='button:contains("Disable proxy and re-open tab")').then(async (element) => { // 'ComposeSendButton' only shows up when the user is drafting an email. It's the best identifier element for the compsoe view.
+    waitForElm(selector='#ppt-widget-root').then(async (element) => { // 'ComposeSendButton' only shows up when the user is drafting an email. It's the best identifier element for the compsoe view.
         // alert("needle!")
         // const text = $('button:contains("Disable proxy and re-open tab")').parents().eq(2).prop("outerHTML")
         const actualtext = document.querySelector('#ppt-widget-root').textContent.split(' ')[8]
         alert(actualtext)
 
-
-        navigator.clipboard.writeText(text);
     });  
 
 }
